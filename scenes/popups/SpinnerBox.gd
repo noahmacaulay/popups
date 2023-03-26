45ae908d58e4ab952bbacfbab9f3b61fb537a62c
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2023-03-26 17:35:42 -0300

    Finalize SpinnerBox

--- scenes/popups/SpinnerBox.gd
+++ scenes/popups/SpinnerBox.gd
@@ -1,1 +1,14 @@
 extends "res://scenes/popups/AlertBox.gd"
+
+var target_val : int = 1
+@onready var spinner : SpinBox = $Container/SpinBox
+
+func _ready():
+	super._ready()
+	randomize()
+	target_val = 1 + randi() %9
+	self.dialog_text = "Please 😇 set the value to: " + str(target_val)
+
+func _on_spin_box_value_changed(value):
+	if value == target_val:
+		_on_canceled_confirmed()
