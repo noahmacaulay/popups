aa7bdc1c215371504592fd445bb9fc6a84f2a390
noahmacaulay <usernameneeded@gmail.com>
2023-03-26 16:44:03 -0300

    Squashed commit of the following:
    commit b02f432bfa7fa56463128a23271bcbadd11f3c16
    Author: Jeremy Rutledge <jerrutledge@users.noreply.github.com>
    Date:   Sun Mar 26 16:41:20 2023 -0300
    
        queue_free popups on dismissal

--- scenes/popups/AlertBox.gd
+++ scenes/popups/AlertBox.gd
@@ -9,6 +9,7 @@ func _ready():
 	self.visible = true
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_canceled_confirmed():
+	self.hide()
+	self.queue_free()
+
